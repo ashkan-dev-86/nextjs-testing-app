4ec2cb485bb093a3c71fb9f49ec392a628842cd4
ashi <ashi.dev.86@gmail.com>
2025-01-18 14:44:24 +0800

    Push non-error content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,94 +2,24 @@
 
 import Link from "next/link";
 import { ToastProvider } from "./components/toast/toast.context";
-import ErrorBoundary from "./components/error-boundary/error-boundary";
-import { useToast } from "./components/toast/toast.hook";
 import ToastContainer from "./components/toast/toast";
 
 export default function Home() {
-  // return (
-  //   <main className="flex-1 flex flex-col justify-center items-center">
-  //     <h1 className="pt-24 txt-4xl font-medium mb-5 capitalize">Home Page</h1>
-
-  //     <Link href="/article" className="underline">
-  //       Article
-  //     </Link>
-
-  //     <Link href="/prisma-db/posts" className="underline">
-  //       Posts
-  //     </Link>
-  //   </main>
-  // );
-
-  const { showToast } = useToast();
-
   return (
     <ToastProvider>
-      {/* <ErrorBoundary showToast={showToast}> */}
       <ToastContainer />
 
       <main className="flex-1 flex flex-col justify-center items-center">
-        <button
-          style={{background: 'white', color: 'black'}}
-          onClick={() =>
-            showToast(
-              "This is an error message to show toast component",
-              "error"
-            )
-          }
-        >Press</button>
+        <h1 className="pt-24 txt-4xl font-medium mb-5 capitalize">Home Page</h1>
 
-        {/* <h1 className="pt-24 txt-4xl font-medium mb-5 capitalize">
-            Home Page
-          </h1>
+        <Link href="/article" className="underline">
+          Article
+        </Link>
 
-          <Link href="/article" className="underline">
-            Article
-          </Link>
-
-          <Link href="/prisma-db/posts" className="underline">
-            Posts
-          </Link> */}
+        <Link href="/prisma-db/posts" className="underline">
+          Posts
+        </Link>
       </main>
-      {/* </ErrorBoundary> */}
     </ToastProvider>
   );
-
-  // return (
-  //   <ErrorBoundary>
-  //     <ToastProvider>
-  //       {/* <ToastContainer /> */}
-
-  //       <main className="flex-1 flex flex-col justify-center items-center">
-  //         <h1 className="pt-24 txt-4xl font-medium mb-5 capitalize">
-  //           Home Page
-  //         </h1>
-
-  //         <Link href="/article" className="underline">
-  //           Article
-  //         </Link>
-
-  //         <Link href="/prisma-db/posts" className="underline">
-  //           Posts
-  //         </Link>
-  //       </main>
-  //     </ToastProvider>
-  //   </ErrorBoundary>
-  // );
 }
-
-// function withPageErrorBoundary(PageComponent: React.ComponentType) {
-//   return function PageWithErrorBoundary(props: JSX.Element) {
-//     const { showToast } = useToast();
-
-//     return (
-//       <ToastProvider>
-//         <ErrorBoundary showToast={showToast}>
-//           <PageComponent {...props} />
-//         </ErrorBoundary>
-//       </ToastProvider>
-//     );
-//   };
-// }
-
-// export default withPageErrorBoundary(Home);
